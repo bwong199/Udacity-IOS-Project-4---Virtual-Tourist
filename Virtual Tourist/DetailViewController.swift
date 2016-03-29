99aef6453d8c968460fe81f70e5a59bc651e540b
bwong199 <bwong199@my.bcit.ca>
2016-03-28 21:23:08 -0600

    Virtual Tourist

---
+++ Virtual Tourist/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  Virtual Tourist
+//
+//  Created by Ben Wong on 2016-03-27.
+//  Copyright © 2016 Ben Wong. All rights reserved.
+//
+
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class DetailViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
--- Virtual Tourist/ViewController.swift
+++ Virtual Tourist/ViewController.swift
@@ -7,19 +7,140 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet var map: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+   
+        locationManager.requestWhenInUseAuthorization()
+        
+        locationManager.startUpdatingLocation()
+        
+        var latitude:CLLocationDegrees = 51.03
+        
+        var longitude:CLLocationDegrees = -114.14
+        
+        var latDelta:CLLocationDegrees = 0.01
+        
+        var lonDelta:CLLocationDegrees = 0.01
+        
+        var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        
+        var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        map.setRegion(region, animated: false)
+        
+        
+        
+        var uilpgr = UILongPressGestureRecognizer(target: self, action: "action:")
+        
+        // 2 seconds
+        
+        uilpgr.minimumPressDuration = 0.5
+        
+        map.addGestureRecognizer(uilpgr)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    func action(gestureRecognizer: UIGestureRecognizer){
+        
+        
+        var touchPoint = gestureRecognizer.locationInView(self.map)
+        
+        var newCoordinate: CLLocationCoordinate2D = map.convertPoint(touchPoint, toCoordinateFromView: self.map)
+        
+        var annotation = MKPointAnnotation()
+        
+        annotation.coordinate = newCoordinate
+        
+        annotation.title = "New Location"
+        
+        annotation.subtitle = "I tapped this"
+        
+    
+        
+        
+        map.addAnnotation(annotation)
+        
+        print(touchPoint)
     }
-
-
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations)
+        
+        var userLocation: CLLocation = locations[0]
+        
+        var latitude = userLocation.coordinate.latitude
+        
+        var longitude = userLocation.coordinate.longitude
+        
+        var latDelta:CLLocationDegrees = 0.01
+        
+        var lonDelta:CLLocationDegrees = 0.01
+        
+        var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        
+        var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        self.map.setRegion(region, animated: false)
+    }
+    
+    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId)
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView?.canShowCallout = true
+            
+            var rightButton: AnyObject! = UIButton.buttonWithType(UIButtonType.DetailDisclosure)
+            rightButton.titleForState(UIControlState.Normal)
+            
+            pinView!.rightCalloutAccessoryView = rightButton as! UIView
+        }
+        else {
+            pinView?.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+    func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == view.rightCalloutAccessoryView {
+            performSegueWithIdentifier("toLocationDetail", sender: view)
+        }
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == "toLocationDetail" )
+        {
+            var vc = segue.destinationViewController as! DetailViewController
+            
+//            ikinciEkran.tekelName = (sender as! MKAnnotationView).annotation!.title
+            
+        }
+        
+    }
+    
+    
 }
 
